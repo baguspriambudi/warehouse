ec57775d24b128480f2848de2bcf8a94a25d2ea2
bagus priambudi <baguspriambudi77@gmail.com>
2020-10-15 08:49:20 +0700

    fix: main.js

--- main.js
+++ main.js
@@ -12,4 +12,31 @@ app.use(express.urlencoded({ extended: true }));
 // DB Connection
 require('./database/connection');
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'system warehouse service up and running',
+    environment: process.env.NODE_ENV,
+    timestamp: new Date(),
+  });
+});
+
+const routeApiV1 = express.Router();
+app.use('/api/v1', routeApiV1);
+
+app.use((req, res, next) => {
+  const error = new Error('not found');
+  error.status = 400;
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    status: error.status || 500,
+    error: error.message,
+  });
+});
+
 app.listen(PORT, console.log(`listening on PORT ${PORT}`));
+module.exports = app;
