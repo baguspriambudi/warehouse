21e8a515cc0c30915e181e354ff8f95c2a6f89fc
bagus priambudi <baguspriambudi77@gmail.com>
2020-10-17 00:16:01 +0700

    modify: config.json to config.js

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('test', 'root', process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: '127.0.0.1',
   dialect: 'mysql',
   operatorsAliases: 0,
