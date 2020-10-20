4bb6e61e66e88a75968ad7ac8bde44f0fbf7195d
bagus priambudi <baguspriambudi77@gmail.com>
2020-10-20 20:06:10 +0700

    fix: migrations/create-user.js

--- migrations/20201020121544-create-user.js
+++ migrations/20201020121544-create-user.js
@@ -28,6 +28,7 @@ module.exports = {
       },
     });
   },
+  // eslint-disable-next-line no-unused-vars
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Users');
   },
