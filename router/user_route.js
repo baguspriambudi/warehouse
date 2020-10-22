77f320eeab8066fef0e4d974489f2a20c3888be6
bagus priambudi <baguspriambudi77@gmail.com>
2020-10-22 11:46:56 +0700

    fix: schema in router/user_route.js

--- router/user_route.js
+++ router/user_route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const userController = require('../controller/user');
-const schema = require('../middleware/Schema');
+const schema = require('../middleware/schema');
 
 const router = express.Router();
 router.post('/create', schema.midUser, userController.createUser);
