0c2edbd26e2e8025935662229e24c2ed9f3dce53
bagus priambudi <baguspriambudi77@gmail.com>
2020-11-02 15:55:38 +0700

    modify: model/User.js

--- models/User.js
+++ models/User.js
@@ -2,15 +2,20 @@ const Sequelize = require('sequelize');
 const sequelize = require('../database/connection');
 
 module.exports = sequelize.define('User', {
-  provider: { type: Sequelize.ENUM('google', 'local'), allowNull: false },
-  local: {
-    id: { type: Sequelize.STRING, autoIncrement: true, primaryKey: true },
-    email: { type: Sequelize.STRING },
-    password: { type: Sequelize.STRING },
-  },
-  google: {
-    socialId: { type: Sequelize.STRING, primaryKey: true },
-    email: { type: Sequelize.STRING },
-    avatar: { type: Sequelize.STRING },
-  },
+  // provider: { type: Sequelize.ENUM('google', 'local'), allowNull: false },
+  // local: {
+  //   id: { type: Sequelize.STRING, autoIncrement: true, primaryKey: true },
+  //   email: { type: Sequelize.STRING },
+  //   password: { type: Sequelize.STRING },
+  // },
+  // google: {
+  //   socialId: { type: Sequelize.STRING, primaryKey: true },
+  //   email: { type: Sequelize.STRING },
+  //   avatar: { type: Sequelize.STRING },
+  // },
+  id: { type: Sequelize.STRING, autoIncrement: true, primaryKey: true },
+  socialId: { type: Sequelize.STRING, primaryKey: true },
+  email: { type: Sequelize.STRING },
+  name: { type: Sequelize.STRING },
+  avatar: { type: Sequelize.STRING },
 });
