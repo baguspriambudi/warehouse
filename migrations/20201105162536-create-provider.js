890d6d3690eaf80b506a690d0fcddeea44ecbc2a
bagus priambudi <baguspriambudi77@gmail.com>
2020-11-05 23:29:13 +0700

    add: provider migrations

---
+++ migrations/20201105162536-create-provider.js
@@ -0,0 +1,31 @@
+/* eslint-disable strict */
+// eslint-disable-next-line strict
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Providers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      provider: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Providers');
+  },
+};
--- models/Provider.js
+++ models/Provider.js
@@ -1,15 +1,24 @@
-// const Sequelize = require('sequelize');
-// const sequelize = require('../database/connection');
-
-module.exports = (Sequelize, sequelize) => {
-  const Provider = sequelize.define('Provider', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    provider: { type: Sequelize.ENUM('google', 'facebook'), allowNull: false },
-  });
-  Provider.associate = (models) => {
-    Provider.hasMany(models.User, {
-      onDelete: 'casecade',
-    });
-  };
-  return Provider;
-};
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Provider extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   Provider.init({
+//     id: DataTypes.INTEGER,
+//     provider: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'Provider',
+//   });
+//   return Provider;
+// };
