ea653049cceb5851df4bef959ba544fc3a266c47
bagus priambudi <baguspriambudi77@gmail.com>
2020-11-15 20:17:18 +0700

    modify: models

--- models/Provider.js
+++ models/Provider.js
@@ -1,27 +1,11 @@
-const { Model } = require('sequelize');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
 
-module.exports = (sequelize, DataTypes) => {
-  class Provider extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Provider.hasMany(models.User, {
-        as: 'users',
-        foreignKey: 'providerId',
-      });
-    }
-  }
-  Provider.init(
-    {
-      name: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: 'Provider',
-    },
-  );
-  return Provider;
+const Provider = sequelize.define('Provider', {
+  name: { type: DataTypes.STRING },
+});
+
+Provider.associate = (models) => {
+  Provider.hasMany(models.User, { foreignKey: 'providerId' });
 };
+module.exports = Provider;
