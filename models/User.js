c8a0fd707f02e8fd25e267e90a5c21f49a67288e
bagus priambudi <baguspriambudi77@gmail.com>
2020-11-17 20:36:47 +0700

    modify: User.js

--- models/User.js
+++ models/User.js
@@ -1,34 +1,3 @@
-// const { Model } = require('sequelize');
-
-// module.exports = (sequelize, DataTypes) => {
-//   class User extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       User.belongsTo(models.Provider, {
-//         as: 'providers',
-//         foreignKey: 'providerId',
-//       });
-//     }
-//   }
-//   User.init(
-//     {
-//       email: DataTypes.STRING,
-//       name: DataTypes.STRING,
-//       providerId: DataTypes.INTEGER,
-//       socialId: DataTypes.INTEGER,
-//     },
-//     {
-//       sequelize,
-//       modelName: 'User',
-//     },
-//   );
-//   return User;
-// };
-
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database/connection');
 
